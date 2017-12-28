2fffafd7a55e33d9e165c90afa8e77c589b89564
blackboat <zygroup1@gmail.com>
2017-12-28 20:03:02 -0200

    download, uploadS3, store

--- market/urls.py
+++ market/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('s3', views.s3, name='s3'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import requests
 import json
+import csv
+import boto.s3
+import sys
+from boto.s3.key import Key
+import datetime
 
 
 def index(request):
@@ -16,9 +22,28 @@ def index(request):
 			tmp = []
 			for i in range(len(data['dates'])):
 				tmp.append([data['dates'][i]] + [data[c][i] for c in currency])
+				if i != len(data['dates']) - 1:
+					cur = data['dates'][i].split(' ')[0]
+					cur = datetime.datetime.strptime(cur, '%Y-%m-%d')
+					next = data['dates'][i+1].split(' ')[0]
+					next = datetime.datetime.strptime(next, '%Y-%m-%d')
+					diff = next - cur
+					if diff.days > 1:
+						for j in range(diff.days-1):
+							next = cur + datetime.timedelta(j+1)
+							tmp.append([next.strftime('%Y-%m-%d %H:%M:%S')] + [data[c][i] for c in currency])
+
+			with open('media/data.csv', 'w') as csv_file:
+				writer = csv.writer(csv_file)
+				writer.writerow(['TimeStamp'] + currency)
+				for row in tmp:
+					writer.writerow(row)
 			context = {
 				'data': tmp,
 				'currency': currency,
+				'start_date': start_date,
+				'end_date': end_date,
+				'interval': interval,
 			}
 		else:
 			context = {
@@ -27,3 +52,27 @@ def index(request):
 		return render(request, 'market/index.html', context)
 
 	return render(request, 'market/index.html')
+
+
+def s3(request):
+	AWS_ACCESS_KEY_ID = 'AKIAJX4LF2MP4X2XUANQ'
+	AWS_SECRET_ACCESS_KEY = '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'
+
+	bucket_name = AWS_ACCESS_KEY_ID.lower() + '-dump'
+	conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
+	try:
+		bucket = conn.get_bucket(bucket_name)
+	except:
+		bucket = conn.create_bucket(bucket_name, location=boto.s3.connection.Location.DEFAULT)
+
+	testfile = "media/data.csv"
+
+	def percent_cb(complete, total):
+		sys.stdout.write('.')
+		sys.stdout.flush()
+
+	k = Key(bucket)
+	k.key = 'my test file'
+	k.set_contents_from_filename(testfile, cb=percent_cb, num_cb=10)
+	return HttpResponse('Success')
